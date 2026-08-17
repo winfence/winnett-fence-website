0df0288f9e12dbe07f8809f86e517cdc332741ed
winfence <robert@winnettoutdoor.services>
2026-08-17 18:08:36 -0400

    Update page.tsx

--- app/areas/easton-ma/page.tsx
+++ app/areas/easton-ma/page.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import NearbyAreasSection from "../../components/NearbyAreasSection";
+import ServiceLinksSection from "../../components/ServiceLinksSection";
 import Link from "next/link";
 import {
   Wrench,
@@ -329,6 +330,7 @@ export default function EastonCityPage() {
         </div>
       </section>
 
+      <ServiceLinksSection />
       <NearbyAreasSection currentTown="Easton" />
 
       {/* FINAL CTA */}
