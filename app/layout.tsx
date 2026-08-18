392522817690a43325ff1b27ad398d700c929185
winfence <robert@winnettoutdoor.services>
2026-08-17 22:16:42 -0400

    Add Click to Call tracking

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,6 +4,7 @@ import "./globals.css";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
 import LocalBusinessSchema from "./components/LocalBusinessSchema";
+import PhoneClickTracker from "./components/PhoneClickTracker";
 import Script from "next/script";
 
 export const metadata: Metadata = {
@@ -82,7 +83,7 @@ export default function RootLayout({
         />
 
         <Header />
-
+        <PhoneClickTracker />
         {children}
 
         <Footer />
